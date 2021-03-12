52658cc1a4e780d0f11d48eb66a223c511a6f09a
Kornelyzas <kornelgaiz@gmail.com>
2021-03-12 08:50:14 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# 1
+integer = 5
+stringas = "Hello"
+floatas = 20.5
+listas = ['jonas', 'petras', 'onute']
+boolas = True
+
+
+#2
+a = ['one', 'two', 'three']
+print(a)
+b = ('one', 'two', 'three')
+print(b)
+c = {'vardas' : 'Jonas', 'amzius' : 25}
+print(c)
+
+
+
+#3
+X = 'kintamasis'
+x = 1
+print(X)
+print(x)
+
+
+#4
+float_data_type = 11.2
+
+
+#5
+def the_function(x):
+    print(x)
+
+x = [1, 2, 3]
+
+the_function(x)
+
+
